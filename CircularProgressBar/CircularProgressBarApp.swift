c93a8456d72eeb3dfe70e2d9c9d4640b08638d5b
byzhkc3 <byzhkc3@gmail.com>
2024-11-05 15:50:54 +0800

    feat: 初始化仓库

---
+++ CircularProgressBar/CircularProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressBarApp.swift
+//  CircularProgressBar
+//
+//  Created by Yali on 2024/11/5.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
